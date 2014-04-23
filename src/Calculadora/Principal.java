9c4a37f5ef79c2587f03343cecedccf18b9522bb
checha9i <checha.db63@gmail.com>
2014-04-22 21:07:25 -0600

    works

--- src/Calculadora/Principal.java
+++ src/Calculadora/Principal.java
@@ -5,9 +5,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -16,41 +14,38 @@ import java.util.Stack;
  */
 public class Principal extends JFrame {
 String ver=" ";
+    double ve=0;
     public static void main(String[] args) throws IOException {
         Principal frame = new Principal();
         frame.setVisible(true);
         frame.setTitle("Calculadora RPN");
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-/*
-        BufferedReader in =
-                new BufferedReader(new InputStreamReader(System.in));
-        for (;;)  {
-            String s = in.readLine();
-            if (s==null)  break;
-            Stack<String> tks = new Stack<String>();
-            tks.addAll(Arrays.asList(s.trim().split("[ \t]+")));
-            if (tks.peek().equals(""))  continue;
-            try  {
-                double r = evalrpn(tks);
-                if (!tks.empty())  throw new Exception();
-                System.out.println(r);
-            }
-            catch (Exception e)  {System.out.println("error");}
-        }
-    */}
+}
     private static double evalrpn(Stack<String> tok) throws Exception  {
         String tk = tok.pop();
         double x,y;
         try  {x = Double.parseDouble(tk);}
         catch (Exception e)  {
             y = evalrpn(tok);  x = evalrpn(tok);
+            if(tk.equals("^2")) y=y*y;
             if      (tk.equals("+"))  x += y;
             else if (tk.equals("-"))  x -= y;
             else if (tk.equals("*"))  x *= y;
             else if (tk.equals("/"))  x /= y;
+            else if(tk.equals("^")) {
+                double temp=x;
+                for(int i=1;i<y;i++){
+
+                                      x=x*temp;
+                                  }
+
+                }
+
             else throw new Exception();
+
         }
+
         return x;
     }
    public Principal()throws IOException {
@@ -71,9 +66,7 @@ String ver=" ";
        cero.setBounds(5, 175, 50, 20);
        contentPane.add(cero);
 
-       JButton mm = new JButton("+-");
-       mm.setBounds(55, 175, 50, 20);
-       contentPane.add(mm);
+
 
         JButton uno = new JButton("1");
        uno.setBounds(5, 195, 50, 20);
@@ -113,7 +106,7 @@ String ver=" ";
 
        //Espacio para mostrar
        final TextField mostrar = new TextField();
-       mostrar.setBounds(5,15,250,150);
+       mostrar.setBounds(5,15,250,40);
        contentPane.add(mostrar);
 
        //operadores
@@ -134,18 +127,44 @@ String ver=" ";
        contentPane.add(div);
 
        JButton igual = new JButton("=");
-       igual.setBounds(155, 195, 50, 20);
+       igual.setBounds(155, 195, 55, 20);
        contentPane.add(igual);
 
        JButton ac = new JButton("AC");
-       ac.setBounds(205, 195, 50, 20);
+       ac.setBounds(210, 174, 50, 40);
        contentPane.add(ac);
+
+       JButton mm = new JButton("+-");
+       mm.setBounds(55, 175, 50, 20);
+       contentPane.add(mm);
+
+       JButton cuadrado = new JButton("x^2");
+       cuadrado.setBounds(105, 175, 55, 20);
+       contentPane.add(cuadrado);
+
+       JButton expn = new JButton("x^n");
+       expn.setBounds(155, 175, 55, 20);
+       contentPane.add(expn);
+
+       JButton raiz = new JButton("√");
+       raiz.setBounds(5, 155, 50, 20);
+       contentPane.add(raiz);
+
+
+       JButton raizn = new JButton("n√");
+       raizn.setBounds(55, 155, 55, 20);
+       contentPane.add(raizn);
+
+       JButton punto = new JButton(".");
+       punto.setBounds(109, 155, 50, 20);
+       contentPane.add(punto);
+
 
         //Accion botones
         uno.addActionListener(new ActionListener(){
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"1 ";
+               ver=ver+" 1";
 mostrar.setText(ver);
            }
        });
@@ -153,7 +172,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-              ver=ver+"2";
+              ver=ver+" 2";
                mostrar.setText(ver);
            }
        });
@@ -161,7 +180,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"3";
+               ver=ver+" 3";
                mostrar.setText(ver);
            }
        });
@@ -169,7 +188,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"4";
+               ver=ver+" 4";
                mostrar.setText(ver);
            }
        });
@@ -177,7 +196,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"5";
+               ver=ver+" 5";
                mostrar.setText(ver);
            }
        });
@@ -185,7 +204,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"6";
+               ver=ver+" 6";
                mostrar.setText(ver);
            }
        });
@@ -193,7 +212,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"7";
+               ver=ver+" 7";
                mostrar.setText(ver);
            }
        });
@@ -201,7 +220,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"8";
+               ver=ver+" 8";
                mostrar.setText(ver);
            }
        });
@@ -209,7 +228,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"9";
+               ver=ver+" 9";
                mostrar.setText(ver);
            }
        });
@@ -217,7 +236,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"+";
+               ver=ver+" +";
                mostrar.setText(ver);
            }
        });
@@ -225,7 +244,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"-";
+               ver=ver+" -";
                mostrar.setText(ver);
            }
        });
@@ -241,7 +260,7 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"*";
+               ver=ver+" *";
                mostrar.setText(ver);
            }
        });
@@ -249,45 +268,134 @@ mostrar.setText(ver);
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"/";
+               ver=ver+" /";
                mostrar.setText(ver);
            }
        });
+
+       cuadrado.addActionListener(new ActionListener() {
+
+           @Override
+           public void actionPerformed(ActionEvent actionEvent) {
+               ver = ver + " ^2";
+               mostrar.setText(ver);
+           }
+       });
+
+       expn.addActionListener(new ActionListener() {
+
+           @Override
+           public void actionPerformed(ActionEvent actionEvent) {
+               ver = ver + " ^";
+               mostrar.setText(ver);
+           }
+       });
+
+       raiz.addActionListener(new ActionListener() {
+
+           @Override
+           public void actionPerformed(ActionEvent actionEvent) {
+               ver = ver + " 2√";
+               mostrar.setText(ver);
+           }
+       });
+
+       raizn.addActionListener(new ActionListener(){
+
+           @Override
+           public void actionPerformed(ActionEvent actionEvent) {
+               ver=ver+" √";
+               mostrar.setText(ver);
+             }});
+       punto.addActionListener(new ActionListener() {
+
+           @Override
+           public void actionPerformed(ActionEvent actionEvent) {
+               ver = ver + " .";
+               mostrar.setText(ver);
+           }
+       });
+        
+
+     final  Stack<String> tok = new Stack<String>();
        igual.addActionListener(new ActionListener() {
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               for (;;)  {
-                   if (ver==null)  break;
-                   Stack<String> tok = new Stack<String>();
+
+
                    tok.addAll(Arrays.asList(ver.trim().split("[ \t]+")));
-                   if (tok.peek().equals(""))  continue;
-                   try  {
+
+                 if (tok.peek().equals("")){tok.pop();}
+                       try  {
                        double r = evalrpn(tok);
                        if (!tok.empty())  throw new Exception();
 
-                       mostrar.setText(ver);
+                       mostrar.setText(String.valueOf(r));
+                           ve=r;
+                           ver=String.valueOf(ve);
                    }
                    catch (Exception e)  {
                        mostrar.setText("error");}
+               File file = new File("salida.txt");
+
+               if (!file.exists()) {
+
+                   try {
+                       if (file.createNewFile()) {
+                           System.out.println("El fichero se ha creado correctamente");
+                       } else {
+
+                           System.out.println("No ha podido ser creado el fichero");
+                       }
+                   } catch (IOException e) {
+                       e.printStackTrace();
+                   }
+               }
+
+
+/*la clave de activar o no la sobreescritura esta en FileOutputStream(file, true) si le ponemos en true entonces agregas al final de la linea */
+
+               BufferedWriter out = null;
+               try {
+                   out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), "UTF8"));
+               } catch (UnsupportedEncodingException e) {
+                   e.printStackTrace();
+               } catch (FileNotFoundException e) {
+                   e.printStackTrace();
+               }
+
+               assert out != null;
+               try {
+                   out.write(ver);
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               try {
+                   out.write("\n");
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               try {
+                   out.close();
+               } catch (IOException e) {
+                   e.printStackTrace();
                }
 
            }
-
-
        });
        cero.addActionListener(new ActionListener() {
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-               ver=ver+"0 ";
+               ver=ver+"0";
                mostrar.setText(ver);
            } });
        mm.addActionListener(new ActionListener() {
 
            @Override
            public void actionPerformed(ActionEvent actionEvent) {
-              int ve=Integer.parseInt(ver);
+         ve=Double.parseDouble(ver);
               if (ve>0){
               ver="-"+ver;
                }
@@ -295,11 +403,12 @@ mostrar.setText(ver);
                       ver="+"+ver;
               }
                mostrar.setText(ver);
-           }
-
-           });
 
 
+           }});
 
 
-   }}
+
+
+   }
+}
